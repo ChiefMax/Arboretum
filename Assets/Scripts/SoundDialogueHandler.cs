6a9daccfb88331d6536d086b6de5729aee72b4e7
Max Manenschijn <maxmanenschijn@gmail.com>
2019-11-28 19:55:33 +0100

    Ready for tomorrows presentation.

---
+++ Assets/Scripts/SoundDialogueHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDialogueHandler : MonoBehaviour
+{
+    public AudioClip[] DialogueSound;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(playEngineSound(DialogueSound));
+    }
+
+    public void PlayHelp()
+    {
+        StartCoroutine(playEngineSound(DialogueSound));
+    }
+
+    IEnumerator playEngineSound(AudioClip[] playThis)
+    {
+        for (int i = 0; i < DialogueSound.Length; i++)
+        {
+            source.PlayOneShot(playThis[i]);
+            yield return new WaitForSeconds(playThis[i].length);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
