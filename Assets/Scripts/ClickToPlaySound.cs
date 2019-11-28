0280dacebb920e045ea3cc81db65db58b123142c
Max Manenschijn <maxmanenschijn@gmail.com>
2019-11-28 13:44:33 +0100

    Did a overhaul of our MVP and made it look better.

--- Assets/Scripts/ClickToPlaySound.cs
+++ Assets/Scripts/ClickToPlaySound.cs
@@ -10,6 +10,10 @@ public class ClickToPlaySound : MonoBehaviour
 
     public GameObject[] elements;
 
+    public ParticleSystem leaf;
+
+    Vector3 pos = new Vector3(0, 0, 0);
+
     string BtnName;
 
     // Start is called before the first frame update
@@ -34,19 +38,30 @@ public class ClickToPlaySound : MonoBehaviour
                         audioSource.clip = audioClips[0];
                         audioSource.Play(0);
                         elements[0].SetActive(true);
+                        LeafesSpawn();
                         break;
                     case "Bird1":
                         audioSource.clip = audioClips[1];
                         audioSource.Play(0);
+                        elements[1].SetActive(true);
+                        LeafesSpawn();
                         break;
                     case "Woodpecker":
                         audioSource.clip = audioClips[2];
                         audioSource.Play(0);
+                        elements[2].SetActive(true);
+                        LeafesSpawn();
                         break;
                     default:
                         break;
                 }
             }
         }
+    }
+
+    void LeafesSpawn()
+    {
+        ParticleSystem firework = Instantiate(leaf, pos, Quaternion.identity);
+        firework.GetComponent<ParticleSystem>().Play();
     }
 }
