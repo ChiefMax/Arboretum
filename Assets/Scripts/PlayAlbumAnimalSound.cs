37da27dcd32dd2cfac937256882c9266ece2ad81
Max Manenschijn <maxmanenschijn@gmail.com>
2019-12-02 15:23:32 +0100

    Minor UI changes.

--- Assets/Scripts/PlayAlbumAnimalSound.cs
+++ Assets/Scripts/PlayAlbumAnimalSound.cs
@@ -10,7 +10,7 @@ public class PlayAlbumAnimalSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
     }
 
     public void PlaySound()
